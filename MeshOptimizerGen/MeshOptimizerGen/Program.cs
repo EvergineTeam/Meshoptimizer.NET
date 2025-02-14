b745604d97d0272290cbef2c7388e2bed6c8bbf5
jcant0n <javiercantonferrero@gmail.com>
2025-02-15 00:03:45 +0100

    First commit, enums and structs are correctly generated.

---
+++ MeshOptimizerGen/MeshOptimizerGen/Program.cs
@@ -0,0 +1,35 @@
+﻿using CppAst;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace MeshOptimizerGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var headerFile = Path.Combine(AppContext.BaseDirectory, "Headers", "meshoptimizer.h");
+            var options = new CppParserOptions
+            {
+                ParseMacros = true,
+            };
+
+            var compilation = CppParser.ParseFile(headerFile, options);
+
+            // Print diagnostic messages
+            if (compilation.HasErrors)
+            {
+                foreach (var message in compilation.Diagnostics.Messages)
+                {
+                    Debug.WriteLine(message);
+                }
+            }
+            else
+            {
+                string outputPath = "..\\..\\..\\..\\..\\Evergine.Bindings.MeshOptimizer\\Generated";
+                CsCodeGenerator.Instance.Generate(compilation, outputPath);
+            }
+        }
+    }
+}
