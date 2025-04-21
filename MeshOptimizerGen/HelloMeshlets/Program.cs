1dc8423c94f53a13b869624c0ce8650c4af7a0b7
jcant0n <javiercantonferrero@gmail.com>
2025-04-21 14:26:06 +0200

    Loaded the native reference manually.

--- MeshOptimizerGen/HelloMeshlets/Program.cs
+++ MeshOptimizerGen/HelloMeshlets/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Evergine.Bindings.MeshOptimizer;
 using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace HelloMeshlets
 {
@@ -7,6 +10,9 @@ namespace HelloMeshlets
     {
         static void Main(string[] args)
         {
+            // This will not be needed when we use a nuget package
+            NativeLibrary.SetDllImportResolver(typeof(Evergine.Bindings.MeshOptimizer.meshopt_Bounds).Assembly, ResolveRuntimes);
+
             Console.WriteLine("Hello, Meshlets!");
             
             uint kMaxVertices = 64;
@@ -16,6 +22,25 @@ namespace HelloMeshlets
             // 1084
             uint maxMeshlets = MeshOptNative.meshopt_buildMeshletsBound(meshIndices, kMaxVertices, kMaxTriangles);    
             
+        }
+
+        private static IntPtr ResolveRuntimes(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            var rid = RuntimeInformation.RuntimeIdentifier;
+            var runtimesFolder = Path.Combine(Path.GetDirectoryName(assembly.Location), $@"runtimes/{rid}/native");
+            if (Directory.Exists(runtimesFolder))
+            {
+                var files = Directory.GetFiles(runtimesFolder, $"*{libraryName}.*");
+                foreach (var file in files)
+                {
+                    if (NativeLibrary.TryLoad(file, out var handle))
+                    {
+                        return handle;
+                    }
+                }
+            }
+
+            return NativeLibrary.Load(libraryName, assembly, searchPath);
         }
     }
 }
