6fc6688384fcdf6299f577a881472d709221e1fe
jorgemagic <jferrero@plainconcepts.com>
2025-04-21 14:05:01 +0200

    - Uploaded simple Hello Meshlet test project

---
+++ MeshOptimizerGen/HelloMeshlets/Program.cs
@@ -0,0 +1,21 @@
+﻿using Evergine.Bindings.MeshOptimizer;
+using System;
+
+namespace HelloMeshlets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Meshlets!");
+            
+            uint kMaxVertices = 64;
+            uint kMaxTriangles = 124;
+            uint meshIndices = 67164;
+
+            // 1084
+            uint maxMeshlets = (uint)MeshOptNative.meshopt_buildMeshletsBound(meshIndices, kMaxVertices, kMaxTriangles);            
+            
+        }
+    }
+}
